e6ef5d7c3dd4cbd623221b83be732cc85b7ba7f2
gutierresk1 <43971263+gutierresk1@users.noreply.github.com>
2020-12-08 11:48:00 -0500

    created br KG

---
+++ HelloWorldMaven/src/main/java/firstmavenapp/App.java
@@ -0,0 +1,13 @@
+package firstmavenapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
