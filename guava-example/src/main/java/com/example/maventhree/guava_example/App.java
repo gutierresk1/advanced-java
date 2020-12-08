a301b330a4c6c1786997fafb6b6b65a45b4f2de4
gutierresk1 <43971263+gutierresk1@users.noreply.github.com>
2020-12-08 13:23:39 -0500

    pushing the maven build

---
+++ guava-example/src/main/java/com/example/maventhree/guava_example/App.java
@@ -0,0 +1,14 @@
+package com.example.maventhree.guava_example;
+import com.google.common.collect.HashMultiset;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Multiset<String> animals = HashMultiset.create();
+    }
+}
